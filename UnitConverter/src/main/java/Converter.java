3ee1d24c03d0f3b8109a1bcd4cf357214e597e55
Seth Knox <seth.r.knox@gmail.com>
2021-05-14 15:18:30 -0500

    added two layer menus

--- UnitConverter/src/main/java/Converter.java
+++ UnitConverter/src/main/java/Converter.java
@@ -7,58 +7,135 @@ public class Converter {
 		int menuSelection = 0;
 		Scanner scanner = new Scanner(System.in);
 		
-		while (menuSelection != 5) {
+		while (menuSelection != 3) {
 			System.out.println("Please select an option:");
-			System.out.println("1. Miles to Kilometers");
-			System.out.println("2. Kilometers to Miles");
-			System.out.println("3. Farenheit to Celsius");
-			System.out.println("4. Celsius to Farenheit");
-			System.out.println("5. Quit");
+			System.out.println("1. Distance convserions");
+			System.out.println("\t Miles to Kilometers");
+			System.out.println("\t Kilometers to Miles");
+			System.out.println("2. Temperature conversions");
+			System.out.println("\t Farenheit to Celsius");
+			System.out.println("\t Celsius to Farenheit");
+			System.out.println("3. Quit");
 			
 			menuSelection = scanner.nextInt();
 			
-			double unit1 = 0;
-			double unit2 = 0;
-			String finalUnit = null;
-			
+			int check = 0;
 			switch(menuSelection) {
 			case 1: {
-				unit1 = collectQuantity("miles", "kilometers", scanner);
-				unit2 = convertMilesToKilometers(unit1);
-				finalUnit = " Kilometer(s)";
+				check = distanceConversions(scanner);
 				break;
 			}
 			case 2: {
-				unit1 = collectQuantity("kilometers", "miles", scanner);
-				unit2 = convertKilometersToMiles(unit1);
-				finalUnit = " Mile(s)";
+				check = temperatureConversions(scanner);
 				break;
 			}
 			case 3: {
-				unit1 = collectQuantity("degrees Farenheit", "Celsius", scanner);
-				unit2 = convertFarenheitToCelsius(unit1);
-				finalUnit = " degrees Celsius";
+				System.out.println("Closed program.");
 				break;
 			}
-			case 4: {
-				unit1 = collectQuantity("degrees Celsius", "Farenheit", scanner);
-				unit2 = convertCelsiusToFarenheit(unit1);
-				finalUnit = " degrees Farenheit";
-				break;
-			}
-			case 5: {
-				break;
-			}
+			default: {}
 			}
 			
-			if (menuSelection != 5) {
-				System.out.println(unit2 + finalUnit);
-			} else {
-				System.out.println("Closed program.");
+			if (check == 1) {
+				menuSelection = 3;
 			}
 		}
 		
 		scanner.close();
+	}
+	
+	public static int temperatureConversions(Scanner s) {
+		System.out.println("Please select an option:");
+		System.out.println("1. Farenheit to Celsius");
+		System.out.println("2. Celsius to Farenheit");
+		System.out.println("3. Quit");
+		
+		int menuSelection = s.nextInt();
+		int check = 0;
+		
+		double unit1 = 0;
+		double unit2 = 0;
+		String startUnit = null;
+		String finalUnit = null;
+		
+		switch(menuSelection) {
+		case 1: {
+			startUnit = "degrees Farenheit";
+			finalUnit = "degrees Celsius";
+			unit1 = collectQuantity(startUnit, finalUnit, s);
+			unit2 = convertFarenheitToCelsius(unit1);
+			break;
+		}
+		case 2: {
+			startUnit = "degrees Celsius";
+			finalUnit = "degrees Farenheit";
+			unit1 = collectQuantity(startUnit, finalUnit, s);
+			unit2 = convertCelsiusToFarenheit(unit1);
+			break;
+		}
+		case 3: {
+			check = 1;
+			break;
+		}
+		default:{
+			return 0;
+		}
+		}
+		
+		if (menuSelection != 3) {
+			System.out.println(unit1 + " " + startUnit + " is equal to " + unit2 + " " + finalUnit);
+		} else {
+			System.out.println("Closed program.");
+		}
+		
+		return check;
+	}
+	
+	public static int distanceConversions(Scanner s) {
+		System.out.println("Please select an option:");
+		System.out.println("1. Miles to Kilometers");
+		System.out.println("2. Kilometers to Miles");
+		System.out.println("3. Quit");
+		
+		int menuSelection = s.nextInt();
+		int check = 0;
+		
+		double unit1 = 0;
+		double unit2 = 0;
+		String startUnit = null;
+		String finalUnit = null;
+		
+		switch(menuSelection) {
+		case 1: {
+			startUnit = "Mile(s)";
+			finalUnit = "Kilometer(s)";
+			unit1 = collectQuantity(startUnit, finalUnit, s);
+			unit2 = convertMilesToKilometers(unit1);
+			break;
+		}
+		case 2: {
+			startUnit = "Kilometer(s)";
+			finalUnit = "Mile(s)";
+			unit1 = collectQuantity(startUnit, finalUnit, s);
+			unit2 = convertKilometersToMiles(unit1);
+			break;
+		}
+		case 3: {
+			check = 1;
+			break;
+		}
+		default:{
+			return 0;
+		}
+		}
+		
+		if (menuSelection != 3) {
+			System.out.println(unit1 + " " + startUnit + " is equal to " + unit2 + " " + finalUnit);
+		} else {
+			System.out.println("Closed program.");
+		}
+		
+		return check;
 	}
 	
 	private static double collectQuantity(String unit1, String unit2, Scanner s) {
